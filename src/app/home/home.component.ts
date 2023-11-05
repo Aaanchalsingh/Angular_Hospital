00b2da91d32c05f500ec0b290a3f975d43ca83e5
Aanchal Singh <aanchalsinghuk@gmail.com>
2023-11-05 12:08:27 +0530

    hello

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,11 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { ScrollDispatcher, CdkScrollable } from '@angular/cdk/scrolling';
-import { trigger, state, style, animate, transition } from '@angular/animations';
-
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  animations: [
-    trigger('animateInViewport', [
-      state('in', style({ opacity: 1, transform: 'translateY(0)' })),
-      state('out', style({ opacity: 0, transform: 'translateY(20px)' })),
-      transition('out => in', animate('0.5s')),
-    ]),
-  ]
+
 })
 export class HomeComponent {
-  @ViewChild('yourElement') element!: ElementRef;
-  elementInViewport = false;
 
-  constructor(private scroll: ScrollDispatcher) {
-    this.scroll.ancestorScrolled(this.element).subscribe(() => {
-      this.elementInViewport = true;
-    });
-  }
 }
