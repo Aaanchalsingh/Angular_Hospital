2cf7f08181fd0ee980690050e381d9cc6073c916
Aanchal Singh <aanchalsinghuk@gmail.com>
2023-11-06 09:14:05 +0530

    latest

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,5 +7,10 @@ import { Component} from '@angular/core';
 
 })
 export class HomeComponent {
-
+  scrollTo(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
